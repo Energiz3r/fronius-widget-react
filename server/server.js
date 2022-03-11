9e1c578705fcd6fcbd3b95698c2c35ca67314e01
Tangles <tim.eastwood@hotmail.com>
2022-03-11 20:07:21 +1100

    server add node_envs

--- server/server.js
+++ server/server.js
@@ -4,15 +4,21 @@ import fetch from "node-fetch";
 import { dirname } from "path";
 import { fileURLToPath } from "url";
 
+if (process.env.NODE_ENV)
+  console.log("NODE_ENV was defined, it was: ", process.env.NODE_ENV);
+if (process.env.PORT)
+  console.log("PORT was defined, it was: ", process.env.PORT);
+
 const dev = !process.env.NODE_ENV || process.env.NODE_ENV === "development";
+const port = process.env.PORT || 4000;
+const subDir = "/build";
+const APIurl = dev
+  ? `http://home.noobs.wtf:${port}/api`
+  : "http://192.168.1.3/solar_api/v1/GetPowerFlowRealtimeData.fcgi";
 
 const app = express();
 const __dirname = dirname(fileURLToPath(import.meta.url));
-const publicPath = path.join(__dirname, "/build");
-const port = process.env.PORT || 4000;
-const APIurl = dev
-  ? "http://home.noobs.wtf:5000/api"
-  : "http://192.168.1.3/solar_api/v1/GetPowerFlowRealtimeData.fcgi";
+const publicPath = path.join(__dirname, subDir);
 
 app.use(express.static(publicPath));
 app.get("", (req, res) => {
@@ -23,5 +29,5 @@ app.get("/api", async (req, res) => {
   const responseResolved = await response.json();
   res.send(responseResolved);
 });
-app.listen(port, () => console.log(`App listening at port: ${port}`));
-console.log("Environment is: ", process.env.NODE_ENV);
+app.listen(port, () => console.log(`PORT: ${port}`));
+console.log("NODE_ENV: ", process.env.NODE_ENV);
